e855342fbd7de21967d2e52a0f47043bcfe9ea2a
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-26 17:34:07 -0300

    inclusao da aula3 e aula4

---
+++ aulas/aula3 - constantes/script.js
@@ -0,0 +1,6 @@
+const canal = "CFB Cursos"; //Declaracao de uma constante. Diferente de uma declaracao var que podemos alterar o seu conteudo a qualquer momento, o const não é possivel altera-la, pois uma vez definida terá o mesmo valor até o fim do script.
+document.write(canal, "<br>")
+
+//Existem varias const já definidas, exemplo a const pi.
+
+document.write(Math.PI)
---
+++ aulas/aula4 - alert/script.js
@@ -0,0 +1,7 @@
+window.alert("CFB Cursos - Curso de JavaScript") //alert é um metodo do objeto window, não há a necessidade de chamar o objeto window para o alert.
+
+//operando com variaveis e constantes
+
+var canal="CFB Cursos";
+var curso="Curso de JavaScript";
+alert(canal + "-" + curso);
