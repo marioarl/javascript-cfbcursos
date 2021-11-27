a044f802f525c3d57c6ff9a9f7fda48ad3f3c331
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-26 21:42:42 -0300

    Alteracao de script.js aula4

--- aulas/aula4 - alert/script.js
+++ aulas/aula4 - alert/script.js
@@ -5,3 +5,14 @@ window.alert("CFB Cursos - Curso de JavaScript") //alert é um metodo do objeto 
 var canal="CFB Cursos";
 var curso="Curso de JavaScript";
 alert(canal + "-" + curso);
+
+//dentro do metodo alert, não funciona os codigos do html, se vc colocar um codigo <br>, simplesmente o alert vai desconsiderar.
+
+var canal1 = "CFB Cursos";
+var curso1 = "Curso de Python";
+const fim = "FIM"
+const separador = " - ";
+const quebra = "<br>" // alert ignora o codigo html e escreve como uma string
+alert(canal1 + separador + curso1 + quebra + fim) 
+
+// para quebrar uma linha dentro do alert, deve-se utilizar \n
