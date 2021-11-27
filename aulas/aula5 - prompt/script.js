5aac75fdad78d228aff96c142e0b4486be72f36d
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-27 08:53:09 -0300

    criacao aula5

---
+++ aulas/aula5 - prompt/script.js
@@ -0,0 +1,6 @@
+//prompt("Digite seu nome", "aqui");//prompt é um metodo do objeto window, não há a necessidade de chamar o objeto window para o prompt. são necessarios 2 parametros dentro do metodo prompt. o 1o. parametro é o texto da caixa e o 2o. é do input text
+
+var nome = prompt("Digite seu nome", "seu nome aqui"); //pode-se colocar uma variavel com a chamada do prompt e será armazenada na var nome. Tambem pode-se colocar uma variavel dentro dos paramentro
+
+document.write(nome);
+
