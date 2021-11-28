6185bd2d5f946a8898f9341029c19ccee93b6552
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-28 09:35:04 -0300

    criacao aula7

---
+++ aulas/aula7 - if else/script.js
@@ -0,0 +1,37 @@
+/*Operadores logicos
+
+Operador	Nome				Exemplo
+>			Maior				10 > 5
+<			Menor				5 < 10
+>=			Maior ou igual		10 >= 10
+<= 			Menor ou igual		10 <= 10
+!=			Diferente			1 != 2
+==			Igualdade			1 == 1
+
+Estrutura do IF
+
+if (teste logico) {
+	bloco
+}
+
+O teste logico sempre irá retornar 1 ==> ture ou 0 ==> false 
+ */
+
+if(10 > 5) {
+	document.write("CFB Cursos")
+}
+
+var v1, v2;
+v1 = 23;
+v2 = 5;
+
+if (v1 > v2) {
+	document.write('É...')
+}
+
+if (v1 == v2) {
+	document.write("Verdadeiro");
+} else {
+	document.write("Falso")
+}
+
