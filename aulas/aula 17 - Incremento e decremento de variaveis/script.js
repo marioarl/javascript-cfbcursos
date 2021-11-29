3b697ffddc20365b37bd542c416556d02d5d1c8a
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-29 11:55:57 -0300

    Criacao da aula 17

---
+++ aulas/aula 17 - Incremento e decremento de variaveis/script.js
@@ -0,0 +1,19 @@
+var num=10;
+
+num+=1; //Incremento de 1 na variavel num 
+num-=1; //Decremento de 1 na variavel num
+num*=2; //Multiplicando por 2 na variavel num
+num++; // Forma simplificada de fazer o incremento de 1 na variavel num
+num--; // Forma simplificada de fazer o decrento de 1 na variavel num
+
+
+
+document.write(num, "<br>");
+document.write(++num, "<br>") //pré incremento
+
+//Inverter o valor de uma variavel de - para + e + para -
+var num2=10;
+
+//num2=num2*-1;
+num2*= -1
+document.write(num2)
