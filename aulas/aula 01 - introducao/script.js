b3f461b8dc74167fcd5f6f93112f76da13199eaa
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-30 17:18:28 -0300

    Update script.js

--- aulas/aula 01 - introducao/script.js
+++ aulas/aula 01 - introducao/script.js
@@ -1,4 +1,4 @@
-document.write("CFB CURSOS"); //Quando uma pagina é carregada em um browser, é criadao automaticamente um objeto chamado document. O metodo write serve apara imprimir algo na tela.
+document.write("CFB CURSOS"); //Quando uma pagina é carregada em um browser, é criado automaticamente um objeto chamado document. O metodo write serve apara imprimir algo na tela.
 
 document.write("<br> <strong>Curso de JavaScript"); //Perceba que este metdodo consegue ler codigo html e o browser entende como um codigo html in-line
 
