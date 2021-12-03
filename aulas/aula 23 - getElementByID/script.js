7e1093b35c9e46a351e03a640ca9ab3bdade7638
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-03 16:25:10 -0300

    Criacao aula23

---
+++ aulas/aula 23 - getElementByID/script.js
@@ -0,0 +1,13 @@
+document.getElementById("texto"); //sintaxe basica do getElementByID
+
+var txt=document.getElementById("texto"); //utilizando uma variavel para utilizar com o getElementById
+
+txt.style.color="red"; //Ao inves de chamarmos o comando todo, colocamos a variavel txt no comeco
+txt.style.fontSize="50px";
+
+var cx = document.getElementById("caixa");
+cx.style.border="1px solid black";
+cx.style.width = "300px";
+cx.style.height = "300px";
+cx.style.backgroundColor = "gray";
+
