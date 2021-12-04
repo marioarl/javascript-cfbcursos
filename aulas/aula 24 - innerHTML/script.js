65c667258df72002a8d461688fd5838e26aa2572
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-03 21:49:46 -0300

    Criacao aula24

---
+++ aulas/aula 24 - innerHTML/script.js
@@ -0,0 +1,8 @@
+var cxt = document.getElementById("cxtexto").value="Curso de JavaScript";
+//alert(cxt);
+
+var cxt1 = document.getElementById("texto").innerHTML="Aula 24 - innerHTML";
+
+document.getElementById("dv").innerHTML="Novo TEXTO"
+
+
