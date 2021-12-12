b51abdb1ad6e2a1d03a8ddf2c07d4281372276c2
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-12 09:50:46 -0300

    Alteracao de linhas no codigo script.js do desafio da aula 34

--- aulas - Parte 2/aula 34 - addEventListener P2/desafio/script.js
+++ aulas - Parte 2/aula 34 - addEventListener P2/desafio/script.js
@@ -1,16 +1,10 @@
 var px = 0;
 var py = 0;
+var obj;
 
-function clicar(){
-	var obj = document.getElementById("dv1");
-	var tecla = event.code;
-	obj.addEventListener("click", clicar)
-	alert("Evento keydown Adicionado")
-	obj.style.backgroundColor = "#f00"
-}
 
 function mover(){
-	var obj = document.getElementById("dv1");
+	obj = document.getElementById("dv1");
 	var tecla = event.key;
 
 	if (tecla === "ArrowLeft") {
@@ -25,14 +19,29 @@ function mover(){
 	} else if (tecla === "ArrowDown") {
 		py += 10;
 		obj.style.top=py+"px";
-	} else if (tecla === "Enter") {
-		alert("Evento keydown removido")
-		this.removeEventListener("keydown", mover);
-		obj.style.backgroundColor = "#fff";
+	} else if (tecla === "Enter"){
+		obj.style.backgroundColor="#fff";
+		window.alert("Evento keydown removido");
+		document.removeEventListener("keydown", mover)
 	}
 	
 }
 
-document.addEventListener("click", clicar);
-document.addEventListener("keydown", mover);
+function addClique(){
+	document.addEventListener("keydown", mover);
+	obj.style.backgroundColor="#f00"
+	alert("Evento keydown iniciado")
 
+}
+
+
+function iniciar(){
+	obj=document.getElementById("dv1");
+	obj.addEventListener("click",addClique);
+	
+
+}
+
+
+
+window.addEventListener("load", iniciar);
