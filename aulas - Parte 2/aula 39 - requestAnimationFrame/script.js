59aaa1bc724d292cc4bac9106c670e846b99a9c0
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-13 09:36:02 -0300

    Criacao script.js

---
+++ aulas - Parte 2/aula 39 - requestAnimationFrame/script.js
@@ -0,0 +1,38 @@
+var jog;
+var vel;
+var dx;
+var dy;
+var px;
+var py;
+var anima;
+
+function inicia(){
+	vel = 5;
+	dx = 1;//iniciar movimentando para a direita
+	dy = 0;
+	px = 0;
+	py = 0;
+	jog = document.getElementById("jogador");
+	jog.addEventListener("click", function(){
+		cancelAnimationFrame(anima);
+	});
+	game();
+}
+
+function game(){
+	px += dx * vel;
+	//py += dy * vel;
+	jog.style.left = px + "px";
+	//jog.style.top = py + "px";
+	if (px > 800){
+		dx = -1;
+	}else if (px < 0){
+		dx = 1;
+	}
+	anima = requestAnimationFrame(game);
+	
+}
+
+
+
+window.addEventListener("load", inicia);
