70c4c6e19b99e5d6c280fe4de3f3ab1ad442ac23
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-16 14:44:21 -0300

    Alteracao script.js

--- aulas - Parte 2/aula 51 - Jogo da velha parte 1/script.js
+++ aulas - Parte 2/aula 51 - Jogo da velha parte 1/script.js
@@ -14,12 +14,87 @@ function jogar(p){
 				if(jogo[0][0] == ""){
 					jogo[0][0] = "X";
 					quemJoga=1;
+				}	
+			break;
+			
+			case 2: 
+				if(jogo[0][1] == ""){
+					jogo[0][1] = "X";
+					quemJoga=1;
 				}
 			break;
+			
+			case 3: 
+				if(jogo[0][2] == ""){
+					jogo[0][2] = "X";
+					quemJoga=1;
+				}
+			break;
+			
+			case 4: 
+				if(jogo[1][0] == ""){
+					jogo[1][0] = "X";
+					quemJoga=1;
+				}
+			break;
+			
+			case 5: 
+				if(jogo[1][1] == ""){
+					jogo[1][1] = "X";
+					quemJoga=1;
+				}
+			break;
+			
+			case 6: 
+				if(jogo[1][2] == ""){
+					jogo[1][2] = "X";
+					quemJoga=1;
+				}
+			break;
+			
+			case 7: 
+				if(jogo[2][0] == ""){
+					jogo[2][0] = "X";
+					quemJoga=1;
+				}
+			break;
+			
+			case 8: 
+				if(jogo[2][1] == ""){
+					jogo[2][1] = "X";
+					quemJoga=1;
+				}
+			break;
+			
+			case 9: 
+				if(jogo[2][2] == ""){
+					jogo[2][2] = "X";
+					quemJoga=1;
+				}
+			break;		
+		}
+		atualizaTabuleiro();	
+	}
+}
+
+function atualizaTabuleiro(){
+	for(var l=0; l < 3; l++){
+		for(var c=0; c < 3; c++){
+			if(jogo[l][c] == "X"){
+				tabuleiro[l][c].innerHTML="X";
+				tabuleiro[l][c].style.cursos="default";
+			}else if(jogo[l][c] == "O"){
+				tabuleiro[l][c].innerHTML="O";
+				tabuleiro[l][c].style.cursos="default";
+			} else{
+				tabuleiro[l][c].innerHTML="";
+				tabuleiro[l][c].style.cursos="pointer";
+
+			}
 
 		}
-
 	}
+
 }
 
 function inicia(){
@@ -30,6 +105,11 @@ function inicia(){
 		["", "", ""],//Corresponde a 2a. linha do jogo da velha
 		["", "", ""]//Corresponde a 2a. linha do jogo da velha
 	];
+	tabuleiro = [
+		[document.getElementById("p1"),document.getElementById("p2"), document.getElementById("p3") ],
+		[document.getElementById("p4"),document.getElementById("p5"), document.getElementById("p6")],
+		document.getElementById("p7"),document.getElementById("p8"), document.getElementById("p9")
+	]
 
 
 }
