f693d6792ab4f2650ef70b12c078f2e79541f546
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-19 11:14:29 -0300

    Criacao script.js

---
+++ aulas - Parte 2/aula 57 - Pong Parte 1/script.js
@@ -0,0 +1,35 @@
+//Elementos - P1 10:50
+var vbtIniciar;
+var Vbola;
+var vcpu;
+var vjogador;
+var vPaineltxtPontos;
+
+//Controle de animacao - P1 12:08
+var game, frames;
+
+//Posições - P1 13:00
+var posBolaX, posBolaY;
+var JogadorX, posJogadorY;
+var posCpuX, posCpuY;
+
+//Direção de acordo com a tecla - P1 15:00
+var dirJy;
+
+//Posicoes inicias - P1 15:27
+var posJogIniY = 180, posCpuIniY = 180, posBolaIniX = 475, posBolaIniY = 240;
+
+//Tamanhos - P1 17:40
+var campoX = 0, campoY = 0, campoW = 960, campoH = 500;
+var barraW = 20, barraH = 140, bolaW=20, bolaH = 20;
+
+//Direcao - P1 19:42
+var bolaX, bolaY;
+var jogY =0; cpuY = 0;
+
+//Velocidade - P1 - 20:26
+var velBola, velCpu, velJogador;
+
+//Controle
+var pontos = 0;
+var tecla;
