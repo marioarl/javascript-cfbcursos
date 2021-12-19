df2c176b1ade9b3b896e74a70ec43a53cb1706d8
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-19 12:04:04 -0300

    Alteracao script.js

--- aulas - Parte 2/aula 57 - Pong Parte 1/script.js
+++ aulas - Parte 2/aula 57 - Pong Parte 1/script.js
@@ -1,6 +1,6 @@
 //Elementos - P1 10:50
 var vbtIniciar;
-var Vbola;
+var vbola;
 var vcpu;
 var vjogador;
 var vPaineltxtPontos;
@@ -10,7 +10,7 @@ var game, frames;
 
 //Posições - P1 13:00
 var posBolaX, posBolaY;
-var JogadorX, posJogadorY;
+var posJogadorX, posJogadorY;
 var posCpuX, posCpuY;
 
 //Direção de acordo com a tecla - P1 15:00
@@ -25,7 +25,7 @@ var barraW = 20, barraH = 140, bolaW=20, bolaH = 20;
 
 //Direcao - P1 19:42
 var bolaX, bolaY;
-var jogY =0; cpuY = 0;
+var cpuY = 0;
 
 //Velocidade - P1 - 20:26
 var velBola, velCpu, velJogador;
@@ -33,3 +33,76 @@ var velBola, velCpu, velJogador;
 //Controle
 var pontos = 0;
 var tecla;
+jogo = false; //P2 06:05
+
+
+//Controle do jogador - P2 16:10
+function controlajog(){
+	if(jogo){
+		posJogadorY += velJogador * dirJy;
+		vjogador.style.top = posJogadorY + "px";
+	}
+}
+
+
+//Controle das teclas - P2 12:34
+function teclaDw(event){
+	tecla = event.key;
+	if(tecla === "ArrowUp"){
+		dirJy = -1;
+	}else if(tecla === "ArrowDown"){
+		dirJy = 1;
+	}
+}
+
+function teclaUp(event){
+	tecla = event.key;
+	if(tecla === "ArrowUp"){
+		dirJy = 0;
+	}else if(tecla === "ArrowDown"){
+		dirJy = 0;
+	}
+}
+
+
+
+
+//Controle da animacao - P2 9:31
+function game(){
+	if (jogo){
+		controlajog();
+	}
+	frames = requestAnimationFrame(game);
+}
+
+
+//Iniciar o jogo - P2 2:16
+function iniciaJogo(){
+	if(!jogo){
+		cancelAnimationFrame(frames);
+		jogo= true;
+		dirJy = 0;
+		posBolaX = posBolaIniX;
+		posBolaY = posBolaIniY;
+		posJogadorY = 0;
+		posCpuY = posCpuIniY;
+		game();
+	}	
+
+}
+
+//Inicializar os componentes do jogo - P2 3:25
+function inicializa(){
+	velBola = velCpu = velJogador = 8;
+	vbtIniciar = document.getElementById("btIniciar");
+	vbtIniciar.addEventListener("click", iniciaJogo);
+	vjogador = document.getElementById("dvJogador");
+	vcpu = document.getElementById("dvCpu");
+	vbola = document.getElementById("dvBola");
+	vPaineltxtPontos = document.getElementById("txtPontos");
+	document.addEventListener("keydown", event => teclaDw(event));
+	document.addEventListener("keyup", event => teclaUp(event)); 
+
+}
+
+window.addEventListener("load", inicializa)
