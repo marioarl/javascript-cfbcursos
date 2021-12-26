1506f27f2143e10c7ac2a9cd492c6cc60ec02d27
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2021-12-25 21:09:20 -0300

    Criacao script.js

---
+++ aulas - Parte 3/aula 69 - Objeto Location p2/script.js
@@ -0,0 +1,9 @@
+//location.assign("p2.html")
+//location.replace("p2.html") //Replace remove a url do historico
+function p2(){
+    location.replace("p2.html");
+}
+
+function rec(){
+    location.reload()
+}
