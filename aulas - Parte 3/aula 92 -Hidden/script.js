78409d1311dcb707f8b044c8df6ea2ae9b5f8807
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-21 14:43:12 -0300

    Criacao script.js

---
+++ aulas - Parte 3/aula 92 -Hidden/script.js
@@ -0,0 +1,23 @@
+
+function mostrar(){
+    for (var i = 1; i <5; i++){
+        document.getElementById("c"+i).hidden=true;
+    }
+    var obj = event.target.dataset.nome;
+    document.getElementById(obj).hidden=false;
+}
+
+function inicia(){
+    for (var i = 1; i <5; i++){
+        document.getElementById("c"+i).hidden=true;
+    }
+    document.getElementById("btnc1").addEventListener("click", mostrar);
+    document.getElementById("btnc2").addEventListener("click", mostrar);
+    document.getElementById("btnc3").addEventListener("click", mostrar);
+    document.getElementById("btnc4").addEventListener("click", mostrar);
+    
+}
+
+
+
+window.addEventListener("load", inicia);
