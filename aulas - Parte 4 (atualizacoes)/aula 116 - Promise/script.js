d6384cf8112407b7e6cdb7829900cb30725d2636
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-02 16:32:58 -0300

    Criacao script.js

---
+++ aulas - Parte 4 (atualizacoes)/aula 116 - Promise/script.js
@@ -0,0 +1,88 @@
+function contar(n){
+    let cont = 0
+    for(let i = 0; i<n; i++){
+        cont++
+    }
+    return cont
+}
+
+function contarT(n){
+    let cont = 0
+    let t=setTimeout(()=>{
+        for(let i = 0; i<n; i++){
+            cont++
+        }
+
+    },2000)
+    
+    return cont
+}
+
+function contarP(n){
+    let cont = 0
+    let p = new Promise((resolve,rejected)=>{//Parametros RESOLVE, REJECT
+        for(let i = 0; i<n; i++){
+            cont++
+        }
+        if(cont == n){
+            resolve(cont)
+        }else{
+            rejected('ERRO')
+        }
+    })
+    return p
+}
+
+function somaPositivos(n1,n2){
+    let p = new Promise((res,rej)=>{
+        if(n1>=0 && n2>=0){
+            res(n1+n2)
+        }else{
+            rej('ERRO')
+        }
+    })
+    return p
+}
+
+//Funcao assincrona
+function contarPT(n){
+    let cont = 0
+    let p = new Promise((resolve,rejected)=>{//Parametros RESOLVE, REJECT
+        let t=setTimeout(()=>{
+            for(let i = 0; i<n; i++){
+                cont++
+            }
+            if(cont == n){
+                resolve(cont)
+            }else{
+                rejected('ERRO')
+            }
+        },2000)
+        
+    })
+    return p
+}
+
+contarPT(10)
+    .then(sucesso => console.log('Sucesso: ' + sucesso))
+    .catch(erro => console.log(erro))
+
+console.log(contar(10000))
+contarP(10)
+    .then(sucesso => console.log('Sucesso: ' + sucesso))
+    .catch(erro => console.log(erro))
+
+somaPositivos(10,2)
+    .then(sucesso => console.log('Sucesso: ' + sucesso))
+    .catch(erro => console.log(erro))
+
+console.log(contarT(10))
+
+
+
+
+
+
+
+
+    
